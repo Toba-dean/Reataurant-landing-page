b6220e3f34bb78c139e778f3eeaba9fb8abf4bce
Sheriff Dean <deans_96@yahoo.com>
2022-02-14 22:25:16 +0100

    done with some part of the app

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,9 +1,21 @@
 import './MenuItem.css'
 
-const MenuItem = () => {
+const MenuItem = ({ title, price, tags }) => {
   return (
-    <div>
-      <h1>Menu.</h1>
+    <div className="app__menuitem">
+      <div className="app__menuitem-head">
+        <div className="app__menuitem-name">
+          <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+        </div>
+        <div className="app__menuitem-dash" />
+        <div className="app__menuitem-price">
+          <p className="p__cormorant">{price}</p>
+        </div>
+      </div>
+
+      <div className="app__menuitem-sub">
+        <p className="p__opensans" style={{ color: '#AAAAAA' }}>{tags}</p>
+      </div>
     </div>
   )
 };
